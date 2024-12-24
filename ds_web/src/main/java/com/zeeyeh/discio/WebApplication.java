10b196bcd00aa17be82ab6568e43d10077d72e6b
LeonKeiran <bemiou_lingqi@163.com>
2024-12-24 20:46:51 +0800

    Project Init

---
+++ ds_web/src/main/java/com/zeeyeh/discio/WebApplication.java
@@ -0,0 +1,18 @@
+package com.zeeyeh.discio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Leon_Keiran
+ * @description 项目入口
+ * @date 2024/12/24/周二 20:39:06
+ * @github <a href="https://github.com/LeonKeiran">https://github.com/LeonKeiran</a>
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
